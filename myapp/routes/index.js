d73cded8be21aab3ac31967021a1919f290fdce0
kukuman98 <khaiyi5893@gmail.com>
2020-07-11 21:58:58 +0800

    initial heroku started way, update project

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { big_title: 'GeneProject',title: 'Our Gene Project index',data : "We have testAPI. Use to URL/testAPI/ \n..." });
 });
 
 module.exports = router;
