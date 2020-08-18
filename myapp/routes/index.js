d5ab032d9484133180089bfc76f61b2a83acd861
kukuman98 <khaiyi5893@gmail.com>
2020-08-18 15:09:16 +0800

    modify index2

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,7 +5,7 @@ data = "We have testAPI,patientAPI, ..."
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { big_title: 'GeneProject',title: 'Our Gene Project index',data : "We have testAPI. Use to URL/testAPI/ \n..." });
+  res.render('index', { big_title: 'GeneProject',title: 'Our Gene Project index',data : data });
 });
 
 module.exports = router;
